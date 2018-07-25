a8ed37505b4cab4c6e6e0b92c70d698591908a2b
Angelo Calvo A <angelo.calvoa@gmail.com>
2018-07-25 16:21:36 -0400

    commit the first estable version with all orm methods

--- index.js
+++ index.js
@@ -1,16 +1,11 @@
-var init = require('./lib/initialize');
+var _initialize = require('./lib/initialize');
+var _defaults = require('./lib/defaults');
 
 module.exports = function multitenant(sails) {
     return {
-        defaults: {
-            __configKey__: {
-               _hookTimeout: 5000, // wait 20 seconds before timing out
-               name: 'Multitenant ORM',
-               //actions: ['create','createEach','find','findOne','update','destroy','findOrCreate','count','stream','archive','avg','sum','removeFromCollection','addToCollection','removeFromCollection','validate','getDatastore'] 
-            }
-        },
+        defaults: _defaults,
         initialize: function(next) {
-            init(sails, next);
+            _initialize(sails, next);
         }
     }
 }
